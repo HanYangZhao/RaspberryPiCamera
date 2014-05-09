3c3a058baed0e928fcac47961a5da1bb109958ca
hyz <zhao_hanyang@hotmail.com>
2014-05-08 20:32:25 -0400

    fixed multiple emails issues

--- captureemail.py
+++ captureemail.py
@@ -10,50 +10,53 @@ import datetime
 import os
 import logging
 
-LOG_FILENAME = '/home/pi/log/python.out'
-logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,)
+filename = sys.argv[1]
+recipients_option = sys.argv[2]
+cc = ""
 
-logging.debug('This message should go to the log file')
+if recipients_option == "a":
+	recipients = ['bobthesheep22@gmail.com ', 'dc_cn@yahoo.com']
+	
+elif recipients_option == "r":
+	recipients = "bobthesheep22@gmail.com"
 
-try:
+elif recipients_option == "z":
+	recipients = "dc_cn@yahoo.com"
 
-	filename = sys.argv[1]
-	gmail_user = "bobthesheep22@gmail.com"
-	gmail_pwd = "u2421mathm50"
+gmail_user = "bobthesheep22@gmail.com"
+gmail_pwd = "u2421mathm50"
 
-	now = datetime.datetime.now()
+now = datetime.datetime.now()
 
-	def mail(to, subject, text, attach):
-	   msg = MIMEMultipart()
+def mail(to, subject, text, attach):
+	msg = MIMEMultipart()
 
-	   msg['From'] = gmail_user
-	   msg['To'] = to
-	   msg['Subject'] = subject
+	msg['From'] = gmail_user
+	msg['To'] = ", ". join(recipients) 
+	msg['Cc'] = cc
+	msg['Subject'] = subject
 
 
-	   msg.attach(MIMEText(text))
+	msg.attach(MIMEText(text))
 
-	   part = MIMEBase('application', 'octet-stream')
-	   part.set_payload(open(attach, 'rb').read())
-	   Encoders.encode_base64(part)
-	   part.add_header('Content-Disposition',
-	           'attachment; filename="%s"' % os.path.basename(attach))
-	   msg.attach(part)
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload(open(attach, 'rb').read())
+	Encoders.encode_base64(part)
+	part.add_header('Content-Disposition',
+	   'attachment; filename="%s"' % os.path.basename(attach))
+    	msg.attach(part)
 
-	   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-	   mailServer.ehlo()
-	   mailServer.starttls()
-	   mailServer.ehlo()
-	   mailServer.login(gmail_user, gmail_pwd)
-	   mailServer.sendmail(gmail_user, to, msg.as_string())
-	   # Should be mailServer.quit(), but that crashes...
-	   mailServer.close()
+	mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+	mailServer.ehlo()
+	mailServer.starttls()
+	mailServer.ehlo()
+	mailServer.login(gmail_user, gmail_pwd)
+	mailServer.sendmail(gmail_user, to, msg.as_string())
+	# Should be mailServer.quit(), but that crashes...
+	mailServer.close()
 
-	mail("bobthesheep22@gmail.com",
-	        "RasPi Cam Motion detected at {}" .format(now.strftime("%I:%M%p on %B %d, %Y")),
-	   "This is a email sent by a RasPi",
-	   filename )
+mail(recipients,
+	"RasPi Cam Motion detected at {}" .format(now.strftime("%I:%M%p on %B %d, %Y")),
+	"This is a email sent by a RasPi",
+	filename )
 
-except:
-   logging.exception('Got exception on main handler')
-   raise
