d678c659f6610d197528e57abd8c9012cee9892f
hyz <zhao_hanyang@hotmail.com>
2014-07-23 01:50:07 -0400

    added templog.html and updated templog.py

--- templog.py
+++ templog.py
@@ -8,14 +8,26 @@ import shutil
 sensor = Adafruit_DHT.AM2302
 pin = 14
 x = 0
-maxEntry = 290
-pollTime = 300
+maxEntry = 96
+pollTime = 1800
+isFilled = False
 
 #Delete all content
 def deleteContent(file):
 	file.seek(0)
 	file.truncate()
 
+def deleteFirstLine(filename):
+    with open(filename , 'a+') as f:
+        lines = f.readlines()
+        if len(lines) >= maxEntry:
+            del lines[0]
+            f.close()
+            with open(filename , 'w') as f:
+                f.writelines(lines)
+                f.close()
+        else:
+            f.close()
 
 while True:
     if x < maxEntry:
@@ -25,27 +37,41 @@ while True:
             temperature = round(temperature , 2)
             humidity = round(humidity , 2)
             data = {"Temperature" : temperature, "Humidity" : humidity, "Time" : time.strftime("%Y/%m/%d_%H:%M:%S")}
-            log = [{"Temperature" : temperature, "Humidity" : humidity, "Time" : time.strftime("%Y/%m/%d_%H:%M:%S")}]
+            #log = [{"Temperature" : temperature, "Humidity" : humidity, "Time" : time.strftime("%Y/%m/%d_%H:%M:%S")}]
 
-            with open('/var/www/js/tempdata.json', 'w') as h:
+            #current temp log
+            with open('/var/www/weatherdata/currenttemp.json', 'w') as h:
                 json.dump(data,h, ensure_ascii=False)
-            #f = open("TempMeasurements","a+" )
-            #g = open("/var/www/TempMeasurements","w")
-            with open('/var/www/js/templog.json' , 'a+' ) as f:
-                num_lines = sum(1 for line in f)
-                if num_lines >= maxEntry:
-                    deleteContent(f)
-                json.dump(log,f, ensure_ascii=False)
-            #f.write('{0:.2f},{1:.2f},{2}\n'.format(temperature,humidity,time.strftime("%H:%M:%S_%d/%m/%Y")))
-            #g.write('{0:.2f},{1:.2f},{2}\n'.format(temperature,humidity,time.strftime("%H:%M:%S_%d/%m/%Y")))
-            x = x + 1
             h.close()
-            #f.close()
+
+	        #Temp/Hum log , cleared every 48 hours
+            with open('/var/www/weatherdata/temphumlog.csv' , 'a+' ) as temphum:
+                if x  == -1:
+                    deleteContent(temphum)
+                temphum.write('{0},{1},{2}\n'.format(temperature,humidity,time.strftime("%H:%M:%S_%d/%m/%Y")))
+	        temphum.close()
+            #Check if tempcycle log and hum cycle log is complete, if so delete first line of each log
+            deleteFirstLine('/var/www/weatherdata/tempcycle.csv')
+            deleteFirstLine('/var/www/weatherdata/humcycle.csv')
+
+	        #Temperature cycled log
+            with open('/var/www/weatherdata/tempcycle.csv' , 'a+' ) as tempcycle:
+	            tempcycle.write('{0},{1}\n'.format(time.strftime('%Y,%m,%d,%H,%M,%S'),temperature))
+       	    tempcycle.close()
+
+	        #Humidity cycled log
+            with open('/var/www/weatherdata/humcycle.csv' , 'a+') as humcycle:
+                humcycle.write('{0},{1}\n'.format(time.strftime('%Y,%m,%d,%H,%M,%S'),humidity))
+
+	        humcycle.close()
+
+            x = x + 1
             time.sleep(pollTime)
     elif x >= maxEntry:
+        isFilled = True
         destination = '/home/pi/sambashare/templog/'
         newdest = destination +  time.strftime("%Y%m%d_%H%M%S")
         shutil.copyfile('/var/www/js/templog.json', newdest )
-        x=0
+        x = -1
 
 
